5a3a20acd05dd0d061ad0a23198098534761459d
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-11-06 21:57:56 +0700

    Add files via upload

---
+++ PBO10119042Latihan40.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Muhammad Rifqu Abdillah
+ *  Nama : Muhammad Rifqu Abdillah
+ *  Kelas: Pbo-IF1
+ *  Nim : 10119042 
+ *  Deskripsi :Program ini berisi program untuk menampilkan kepribadian
+ *                     user berdasarkan warna 
+ */
+public class PBO10119042Latihan40 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKepribadian kepribadian = new WarnaKepribadian();
+        kepribadian.inputWarna();
+        kepribadian.inputNama();
+        kepribadian.cekKepribadian();
+    }
+    
+}
